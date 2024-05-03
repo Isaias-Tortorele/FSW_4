a1584e914db56080587f6f7530d551d74ec2ee29
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-05-02 23:25:07 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/category-list";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,7 +11,21 @@ export default function Home() {
         <Search />
       </div>
 
-      <CategoryList />
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src={"/promo-banner-01.png"}
+          alt="Até 30% em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
     </>
   );
 }
