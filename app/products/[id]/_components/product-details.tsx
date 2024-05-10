251ec378f62cdd685c38b82ed4bf0bb261d56c32
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-05-10 14:54:45 -0300

    fix: show delivery time on product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -108,7 +108,7 @@ const ProductDetails = ({
         <Card className="mt-6 flex justify-around px-5 py-3 ">
           {/* CUSTO */}
           <div className="flex  flex-col items-center">
-            <div className="flex items-center text-muted-foreground">
+            <div className="flex items-center gap-1 text-muted-foreground">
               <span className="text-xs">Entrega</span>
               <BikeIcon size={14} />
             </div>
@@ -123,17 +123,14 @@ const ProductDetails = ({
 
           {/* TEMPO */}
           <div className="flex  flex-col items-center">
-            <div className="flex items-center text-muted-foreground">
+            <div className="flex items-center gap-1 text-muted-foreground">
               <span className="text-xs">Tempo</span>
               <TimerIcon size={14} />
             </div>
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-sm font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className="text-sm font-semibold">Grátis</p>
-            )}
+
+            <p className="text-sm font-semibold">
+              {product.restaurant.deliveryTimeMinutes} min
+            </p>
           </div>
         </Card>
       </div>
